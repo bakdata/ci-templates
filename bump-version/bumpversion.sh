a7ae1920bb34f8065e16492645d9b4c3e8be95d1
yannick.roeder <yannick.roeder@bakdata.com>
2021-11-23 14:55:26 +0100

    Add composite actions for helm

---
+++ bump-version/bumpversion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set default to patch and convert to lowercase
+releaseType=$(echo "${1:-patch}" | awk '{print tolower($0)}')
+
+if [[ "${releaseType}" == "major" || "${releaseType}" == "minor" || "${releaseType}" == "patch" ]]; then
+    echo "Bumping version with release type ${releaseType}"
+    bump2version ${releaseType}
+else
+    echo "ERROR: Only major, minor or patch is allowed for input releaseType."
+    exit 1
+fi
