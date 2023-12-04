a8035851c3502dc265f653276617b94ba5351ee9
MichaelKora <michael.koras0@gmail.com>
2023-12-04 12:52:46 +0100

    Test precommit

--- generate-doc.py
+++ generate-doc.py
@@ -120,7 +120,6 @@ def run():
                             "tmp_output": tmp_docu_output_workflow})
     print_colored(
         "--------------------------------------------------------", Colors.BLUE)
-
     # Correction
     need_updates = []
     for entry in changes:
