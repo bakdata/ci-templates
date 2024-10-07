f4b42847ca2620cdb36760c2465f63835b2bfb03
Jan Max Tiedemann <jan.max.tiedemann@bakdata.com>
2024-10-07 02:08:43 +0200

    feat: add parse action

---
+++ actions/parse-secrets-definitions/main.py
@@ -0,0 +1,43 @@
+import os
+import re
+
+# CAVEAT: will only work for one project at a time
+# might be problematic if we want to inject secrets from multiple projects
+
+# Set the output value by writing to the outputs in the Environment File, mimicking the behavior defined here:
+#  https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+def set_github_action_output(output_name, output_value, delim=''):
+    f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
+    f.write(f'{output_name}<<!!!{output_value}!!!') # ATTENTION: this might lead to problems if the output value contains the delimiter, which will not happen in this program but dont just copy this and expect it to work
+    f.close()    
+
+
+# removee special characters and replace with underscores, succevive special characters are replaced with a single underscore
+# convert to uppercase
+# if the secret would end in an underscore, remove it
+# format: SECRET_NAME:PROJECT_NAME/SECRET_NAME/VERSION
+def parse_secret(secret, project_name):
+    components = secret.split("/")
+    if len(components) > 2:
+        raise ValueError(f"Invalid secret definition: {secret}, not in the format 'secret_name/<version>'")
+    secret_name = re.sub('[^0-9a-zA-Z]+', '_', components[0]).upper().rstrip("_")
+    out = f"{secret_name}:{project_name}/{components[0]}"
+    if len(components) == 2:
+        out += f"/{components[1]}"
+    return out
+
+def main():
+    input_secrets = os.environ["INPUT_SECRETS_TO_INJECT"].splitlines()
+    gcp_project = os.environ["INPUT_PROJECT_NAME"]
+
+    output = ""
+    for secret in input_secrets:
+        output += parse_secret(secret, gcp_project) + "\n"
+
+
+    set_github_action_output('secrets-list', output)
+
+
+if __name__ == "__main__":
+    main()
+    # print(parse_secret("alksdjoioe4j####@!@#!1123asdlolw!!asd!!print(/1", "project"))
